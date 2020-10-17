28624e6e7c6a51510d28e2bb503bd6744c445d8d
Vanderlei Silva <vanderlei_nsilva@hotmail.com>
2020-10-16 22:17:01 -0300

    Criando operacao de instanciacao

--- src/main/java/com/vandao/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/vandao/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.vandao.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,10 +19,9 @@ public class CategoriaResource {
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		
+	
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-		
 	}
 	
 }
